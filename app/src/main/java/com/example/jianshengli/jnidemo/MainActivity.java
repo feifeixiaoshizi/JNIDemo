7bc0fa2ff8621e1aceafcb4b3a945daaed12eba3
feifeixiaoshizi <18713027427@163.com>
2018-04-15 11:53:14 +0800

    test revert

--- app/src/main/java/com/example/jianshengli/jnidemo/MainActivity.java
+++ app/src/main/java/com/example/jianshengli/jnidemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 //添加了测试commit的注释
 //练习切换分支,创建分支以及push到分支
+//测试revert
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
